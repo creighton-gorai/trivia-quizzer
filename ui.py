646113777f3870410a0694b50bcef55bda3771d5
Creighton Gorai <creighton-gorai@users.noreply.github.com>
2023-12-23 19:35:15 -1000

    Adding UI elements

--- ui.py
+++ ui.py
@@ -1,2 +1,32 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        # Create window
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg='white')
+
+        self.question = self.canvas.create_text(150, 125, text='The question goes here', font=('Arial', 20, 'italic'))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        # Create True button
+        self.true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=self.true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        # Create False button
+        self.false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=self.false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.window.mainloop()
